30bfe933803b80f9508eaefd6d055121a78383ec
vaikkko <victor3blm@gmail.com>
2023-10-06 17:01:50 +0100

    .php updated

--- ut2/a1/files/Calculadora.php
+++ ut2/a1/files/Calculadora.php
@@ -3,20 +3,20 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="styles.css"/>
+    <link rel="php" href="calculadora.php">
     <title>Aplicación PHP</title>
   </head>
 
   <body>
     <div class="Calculadora">
-    <h1>Calculadora en entorno nativo</h1>
-
-    <form action="/action_page.php">
+    <h1>Calculadora en entorno dockerizado/nativo</h1>
+    <form action="calculadora.php" method="post">
       <label for="fname">Valor 1: </label>
-      <input type="text" id="fname" name="fname" /><br />
+      <input type="text" id="valor1" name="valor1" class="valor1"/><br />
 
       <label for="lname">Valor 2: </label>
-      <input type="text" id="lname" name="lname" /><br />
+      <input type="text" id="valor2" name="valor2" class="valor2"/><br />
 
       <label for="operacion">Operación:</label>
       <select name="operacion" id="operacion">
@@ -30,28 +30,27 @@
 
       <button type="submit">Calcular</button>
     </form>
-    
+
     </div>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $valor1 = $_POST["valor1"];
     $valor2 = $_POST["valor2"];
     $operacion = $_POST["operacion"];
-    $resultado = 0;
 
     switch ($operacion) {
         case "sumar":
-            $resultado = $valor1 + $valor2;
+            echo $valor1 + $valor2;
             break;
         case "restar":
-            $resultado = $valor1 - $valor2;
+            echo $valor1 - $valor2;
             break;
         case "multiplicar":
-            $resultado = $valor1 * $valor2;
+            echo $valor1 * $valor2;
             break;
         case "dividir":
             if ($valor2 != 0) {
-                $resultado = $valor1 / $valor2;
+                echo $valor1 / $valor2;
             } else {
                 echo "<p id='resultado' style='color: red;'>No se puede dividir por 0.</p>";
             }
